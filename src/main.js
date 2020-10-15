2b6b380518be93be5c6b9b77275d8839843d71b1
vellons <vellons@users.noreply.github.com>
2020-10-15 11:07:33 +0200

    Add apex-charts and first test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.config.productionTip = false
+
+Vue.component('apexchart', VueApexCharts)
 
 new Vue({
   render: h => h(App),
